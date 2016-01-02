a939679c63580ddba2c06297147b0c5a172fb7d0
rgan <rganeshan@2u.com>
2016-01-02 18:36:23 -0500

    Add logging

--- tasks/gh_release_with_assets_task.js
+++ tasks/gh_release_with_assets_task.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
     grunt.registerTask('gh_release_with_asset', function(args) {
         var done = this.async();
         var options = this.options();
+        grunt.log.ok("Using tag for release:" + options.tag);
         new Releaser().release_with_asset(options, done);
     });
 };
